3320deab887c7e8383b57391c1f678b8082ede94
Mdromi <mdromi14@gmail.com>
2023-02-12 22:54:50 +0600

    dynamic local clock

--- projects/track-zone/src/App.jsx
+++ projects/track-zone/src/App.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import LocalClock from "./components/local-clock/index";
 import ClockList from "./components/clock-list/index";
 import useClock from "./hooks/useClock";
 
-import testDate from "./test/testDate";
+
+const LOCAL_CLOCK_INIT = {
+	title: 'My Clock',
+	timezone: '',
+	offset: 0,
+	date: null,
+};
 
 function App() {
-  testDate();
+  const [localClock, setLocalClock] = useState({...LOCAL_CLOCK_INIT});
+
+  const updateLocalClock = data => {
+    setLocalClock({
+      ...localClock,
+      ...data,
+    })
+  }
   return (
+    
     <div className="App">
-      <LocalClock />
+      <LocalClock clock={localClock} updateClock={updateLocalClock}/>
       <ClockList />
     </div>
   );
