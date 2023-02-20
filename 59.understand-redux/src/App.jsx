fbcafd1e9954550017566869e2b6b35a2a3cbe73
Mdromi <mdromi14@gmail.com>
2023-02-20 22:13:37 +0600

    understand react-hook-form

--- 59.understand-redux/src/App.jsx
+++ 59.understand-redux/src/App.jsx
@@ -1,17 +1,73 @@
-import Count from "./components/Count";
-import DecrementButton from "./components/DecrementButton";
-import History from "./components/History";
-import IncrementButton from "./components/IncrementButton";
+import { Input } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+
+const InputText = ({ name, label, ...props }) => (
+  <div>
+    <label htmlFor={name}>{label} </label>
+    <input type={name} id={name} {...props} />
+  </div>
+);
 
 function App() {
-  //   const state = useSelector((state) => state);
-  //   console.log("All Store", state);
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, touchedFields },
+  } = useForm({
+    reValidateMode: "onSubmit",
+    defaultValues: { name: "", email: "", age: "" },
+  });
+
+  //   console.log("watch", watch());
+  //   console.log("errors", errors);
+  //   console.log("touchedFields", touchedFields);
+
+  const onValid = (data) => {
+    console.log("onValid", data);
+    reset();
+  };
+
+  const onInValid = (data) => {
+    console.log("onInValid", data);
+  };
+
   return (
     <div className="App">
-      <Count />
-      <IncrementButton />
-      <DecrementButton />
-      <History />
+      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit(onValid, onInValid)}>
+        <Controller
+          control={control}
+          name="name"
+          render={({ field }) => (
+            <InputText name={"name"} label="Name" {...field} />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="email"
+          render={({ field }) => (
+            <InputText name={"email"} label="Email" {...field} />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="age"
+          render={({ field }) => (
+            <InputText name={"age"} label="Age" {...field} />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="number"
+          render={({ field }) => <Input {...field} />}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
--- 59.understand-redux/src/main.jsx
+++ 59.understand-redux/src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { StoreProvider } from "easy-peasy";
-
-import store from "./store";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StoreProvider store={store}>
+  <>
     <App />
-  </StoreProvider>
+  </>
 );
+
+// for store 2
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <StoreProvider store={store}>
+//     <App />
+//   </StoreProvider>
+// );
 
 // For store 1
 // import store from "./store2";
